64ce2d5b161a4ebd18c0765d44c51b7d74c1c339
Ali-Khadra <alikhadra15@gmail.com>
2022-01-29 20:32:42 +0200

    Asfar

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss';
+import './css/style.css';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/js/all.min';
